fe98d0a072f9c556149d997789f2e0b80a2b171d
joshuaphilip <joshuaphilip97@gmail.com>
2020-11-04 18:54:04 -0600

    Updated Driver

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+
+public class Driver {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Compare x = new Compare();
+		x.remove("2010-10-30");
+		
+
+	}
+
+}
